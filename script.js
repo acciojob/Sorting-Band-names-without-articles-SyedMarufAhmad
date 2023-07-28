4a3644273f238e26e40bfdffe6a802a3b2bfb4c0
SyedMarufAhmad <135939210+SyedMarufAhmad@users.noreply.github.com>
2023-07-28 21:42:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-//your code here
-
-// suppose an array that contains a list of popular tourise spots
-let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
-
-// So here the lexicographically sorted order is (without articles)---
-touristSpots = ['Tajmahal', 'Victoria Memorial', 'The Virupaksha Temple'];
+let bandNames=['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal']
+let articles = ['A', 'AN', 'THE']
+ 
+	bandNames.sort((a,b) => (strip(a)> strip(b))? 1: -1)
+	const ref= document.getElementsByTagName("ul")[0] 
+	for (let i=0;i<bandNames.length; i++){
+		const li = document.createElement("li")
+		li.innerText= bandNames [i]
+		ref.append(li)
+	}
+function strip(word){
+	let arr = word.split(" ")
+	let s=""
+	for(let i=0;i<arr.length;i++){
+if(articles.indexOf(arr[i].toUpperCase()) === -1){
+	s=s+arr[i]
+}
+}
+return s.trim()
+}
